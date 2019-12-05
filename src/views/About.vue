386cf6d7b3a57bbaeb2220948aba47d5daf57c87
ranghild <arfrumo@gmail.com>
2019-12-06 04:54:14 +0900

    UPDATE :: ipify

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,40 +1,29 @@
 <template>
-  <div>
-    <h3>{{ userAgent }}</h3>
-    <h3>{{ ipAddress }}</h3>
-  </div>
+  <div></div>
 </template>
 
 <script>
 export default {
-  metaInfo: {
-    meta: [
-      { property: "og:title", content: "test" },
-      { property: "og:image", content: "test" },
-      { property: "og:description", content: "test" }
-    ]
-  },
   data: () => ({
     userAgent: window.navigator.userAgent,
     ipAddress: undefined
   }),
-  mounted: async function() {
+  created: async function() {
     try {
-      let response = await this.$axios.get("https://api.ipify.org?format=json");
-      this.ipAddress = response.data.ip;
+      await this.$axios.get("https://api.ipify.org?format=json").then(response => {
+        this.ipAddress = response.data.ip;
+      });
 
-      response = await this.$axios.get(
+      let response = await this.$axios.get(
         `${process.env.VUE_APP_BACKEND_URL}/api/v1/trap/${this.$route.params.key}`
       );
 
-      await this.$axios.post(
-        `${process.env.VUE_APP_BACKEND_URL}/api/v1/test/`,
-        {
-          shorten_key: this.$route.params.key,
-          ip_address: this.ipAddress,
-          user_agent: this.userAgent
-        }
-      );
+      this.$axios.post(`${process.env.VUE_APP_BACKEND_URL}/api/v1/test/`, {
+        shorten_key: this.$route.params.key,
+        ip_address: this.ipAddress,
+        user_agent: this.userAgent
+      });
+
       window.location.href = response.data.target_url;
     } catch (error) {
       /* eslint-disable no-console */
--- src/views/MakeDionaea.vue
+++ src/views/MakeDionaea.vue
@@ -1,5 +1,10 @@
 <template>
   <v-app id="inspire">
+    <v-app-bar app clipped-right color="deep-purple" dark>
+      <v-toolbar-title>Toolbar</v-toolbar-title>
+      <v-spacer />
+      <v-switch hide-details v-model="$vuetify.theme.dark"></v-switch>
+    </v-app-bar>
     <v-content>
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
@@ -49,19 +54,39 @@
               </v-card-actions>
             </v-card>
           </v-col>
+        </v-row>
+        <v-row align="center" justify="center">
           <v-col cols="12" sm="8" md="7" lg="5">
             <v-card>
               <template v-for="item in trapList">
-                <v-list-item :key="item.shorten_key" @click="detail()">
+                <v-list-item three-line :key="item.shorten_key" @click="detail(item.shorten_key)">
                   <v-list-item-content>
                     <v-list-item-title>https://{{ base_url }}/trap/{{ item.shorten_key }}</v-list-item-title>
                     <v-list-item-subtitle>{{ item.target_url }}</v-list-item-subtitle>
+                    <v-list-item-subtitle>{{ item.created_at }}</v-list-item-subtitle>
                   </v-list-item-content>
                 </v-list-item>
               </template>
             </v-card>
           </v-col>
         </v-row>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="7" lg="5">
+            <v-card>
+              <template v-for="item in information">
+                <v-list-item three-line :key="item.id">
+                  <v-list-item-content>
+                    <v-list-item-title>{{ item.ip_address }}</v-list-item-title>
+                    <v-list-item-subtitle>{{ item.user_agent }}</v-list-item-subtitle>
+                    <v-list-item-subtitle>{{ item.region_name }} {{ item.city }}</v-list-item-subtitle>
+                    <v-list-item-subtitle>{{ item.accessed_at }}</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </template>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row align="center" justify="center"></v-row>
       </v-container>
     </v-content>
   </v-app>
@@ -70,6 +95,7 @@
 <script>
 export default {
   data: () => ({
+    dark: false,
     form: false,
     base_url: document.domain,
     jwt:
@@ -78,6 +104,7 @@ export default {
     trapURL: undefined,
     expiration: ["10min", "20min", "30min", "1hour"],
     targetURL: undefined,
+    information: undefined,
     rules: [
       value => !!value || "Required",
       value => {
@@ -125,7 +152,14 @@ export default {
       }
     },
 
-    detail: function() {},
+    detail: function(shorten_key) {
+      this.$axios
+        .get(`${process.env.VUE_APP_BACKEND_URL}/api/v1/test/${shorten_key}`)
+        .then(response => {
+          this.information = response.data;
+        });
+      // .catch(error => {});
+    },
 
     getJWT: function() {
       this.$axios
