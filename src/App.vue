b7fd4a1ff7b6ed9f7268a97122e81984d968f1ca
ranghild <arfrumo@gmail.com>
2019-12-07 15:14:35 +0900

    gitflow-feature-stash: djongo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <v-app>
-    
     <v-content>
       <router-view />
     </v-content>
@@ -10,9 +9,15 @@
 <script>
 export default {
   name: "App",
+  created: function() {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      this.$vuetify.theme.dark = true;
+    }
+  },
 
   components: {},
 
   data: () => ({})
 };
 </script>
+
