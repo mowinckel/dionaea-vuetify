d9a33b07c51d52747212a301b9739f5600a08cd3
ranghild <arfrumo@gmail.com>
2019-12-09 02:48:10 +0900

    DEL :: vue-meta

--- src/main.js
+++ src/main.js
@@ -3,12 +3,10 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
-import VueMeta from 'vue-meta'
 import store from './store'
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
-Vue.use(VueMeta)
 
 new Vue({
   router,
