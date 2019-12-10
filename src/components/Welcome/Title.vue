3a47b54b82adcf2f0776886e567629c8dab82ee8
ranghild <arfrumo@gmail.com>
2019-12-10 17:22:10 +0900

    UPDATE :: ui

--- src/components/Welcome/Title.vue
+++ src/components/Welcome/Title.vue
@@ -2,7 +2,7 @@
   <v-row align="center" justify="center">
     <v-col lg="5" sm="6">
       <v-img class="white--text align-end" height="160px" src="../../assets/logo.png" contain></v-img>
-      <v-card color="deep-purple accent-3" dark>
+      <v-card>
         <v-card-text class="display-3 text--white text-center">Dionaea</v-card-text>
       </v-card>
     </v-col>
